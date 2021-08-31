023c2e7741563a1a61a40193df720204769f8758
blakes24 <blakes24@gmail.com>
2021-08-30 19:07:00 -0700

    Add ghosts and health bar

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ const config = {
   physics: {
     default: "matter",
     matter: {
-      debug: true,
+      // debug: true,
     },
   },
   scene: {
@@ -30,6 +30,7 @@ const gameState = {
   jumps: 0,
   score: 0,
   scoreText: null,
+  health: 100,
 };
 
 // load game assets
@@ -37,14 +38,15 @@ function preload() {
   this.load.image("background", "assets/Background_0.png");
   this.load.image("background2", "assets/Background_1.png");
   this.load.image("tiles", "assets/tiles.png");
+  this.load.image("mid-tiles", "assets/mid-bg.png");
   this.load.tilemapTiledJSON("tilemap", "assets/graveyard.json");
 
   this.load.spritesheet(
     "witch",
     "assets/witch.png",
     {
-      frameWidth: 25,
-      frameHeight: 45,
+      frameWidth: 26,
+      frameHeight: 48,
     },
     0
   );
@@ -70,23 +72,32 @@ function preload() {
 }
 
 function create() {
-  const bg = this.add.image(300, 200, 'background').setScrollFactor(0);
-  const bg2 = this.add.image(300, 200, 'background2').setScrollFactor(0);
+  const bg = this.add.image(300, 200, "background").setScrollFactor(0);
+  const bg2 = this.add.image(300, 200, "background2").setScrollFactor(0.02);
+  const bg3 = this.add.image(900, 200, "background2").setScrollFactor(0.02);
   bg.setDisplaySize(600, 400);
   bg2.setDisplaySize(600, 400);
+  bg3.setDisplaySize(600, 400);
 
   const map = this.make.tilemap({ key: "tilemap" });
-  const tileset = map.addTilesetImage("gravesheet", "tiles");
-  const ground = map.createLayer("ground", tileset);
+  const tileset1 = map.addTilesetImage("gravesheet", "tiles");
+  const tileset2 = map.addTilesetImage("mid-bg", "mid-tiles");
+  const midground = map.createLayer("midground", tileset2, 0, -500);
+  const ground = map.createLayer("ground", tileset1);
+
+  midground.setScrollFactor(0.3, 0.4);
   ground.setCollisionByProperty({ collides: true });
 
-  this.cameras.main.setBounds(0, 0, 50 * 32, 20 * 32);
-  this.matter.world.setBounds(0, 0, 50 * 32, 20 * 32);
+  this.cameras.main.setBounds(0, 0, 6400, 640);
+  this.matter.world.setBounds(0, 0, 6400, 640);
+
+  gameState.graphics = this.add.graphics().setScrollFactor(0);
+  setHealth(gameState.health);
 
   // player animations for walking and idle
   this.anims.create({
     key: "run",
-    frames: this.anims.generateFrameNumbers("witch", { start: 0, end: 3 }),
+    frames: this.anims.generateFrameNumbers("witch", { start: 0, end: 7 }),
     frameRate: 10,
     repeat: -1,
   });
@@ -97,11 +108,20 @@ function create() {
     frameRate: 20,
   });
 
+  this.anims.create({
+    key: "ghost",
+    frames: this.anims.generateFrameNumbers("ghost", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
   //  The score
-  gameState.scoreText = this.add.text(20, 20, "score: 0", {
-    fontSize: "24px",
-    fill: "#fff",
-  }).setScrollFactor(0);
+  gameState.scoreText = this.add
+    .text(20, 20, "score: 0", {
+      fontSize: "24px",
+      fill: "#fff",
+    })
+    .setScrollFactor(0);
 
   const objectLayer = map.getObjectLayer("objects");
 
@@ -113,7 +133,7 @@ function create() {
       case "start": {
         gameState.player = this.matter.add
           .sprite(x, y, "witch")
-          .setScale(2)
+          .setScale(1.7)
           .setFixedRotation();
 
         gameState.player.setOnCollide((data) => {
@@ -129,12 +149,14 @@ function create() {
                 collectPotion(gameObj);
                 break;
               }
+              case "ghost": {
+                takeDamage();
+                break;
+              }
             }
           }
         });
 
-        this.matter.add.sprite(x + 50, y, "ghost").setFixedRotation();
-        
         this.cameras.main.startFollow(gameState.player);
 
         break;
@@ -142,11 +164,27 @@ function create() {
 
       case "potion": {
         const potion = this.matter.add
-          .sprite(x, y, "potion", 0, { isStatic: true, isSensor: true })
+          .sprite(x, y, "potion", 0, {
+            isStatic: true,
+            isSensor: true,
+          })
           .setScale(1.5);
 
         potion.setData("type", "potion");
 
+        break;
+      }
+      
+      case "ghost": {
+        const ghost = this.matter.add
+          .sprite(x, y, "ghost", 0, {
+            isStatic: true,
+            isSensor: true,
+          }).setFixedRotation();
+
+        ghost.setData("type", "ghost")
+        ghost.play("ghost")
+        
         break;
       }
     }
@@ -159,7 +197,7 @@ function create() {
 
 function update() {
   // add controls
-  const speed = 10;
+  const speed = 4;
   if (gameState.cursors.left.isDown) {
     gameState.player.setVelocityX(-speed);
     gameState.player.flipX = true;
@@ -176,7 +214,7 @@ function update() {
   const upJustPressed = Phaser.Input.Keyboard.JustDown(gameState.cursors.up);
 
   if (upJustPressed) {
-    if (gameState.jumps < 2) {
+    if (gameState.jumps < 1) {
       gameState.player.setVelocityY(-10);
       gameState.player.anims.play("idle");
       gameState.jumps++;
@@ -190,5 +228,23 @@ function collectPotion(potion) {
   //  Add and update the score
   gameState.score += 10;
   gameState.scoreText.setText(`score: ${gameState.score}`);
-  console.log(gameState.score);
+}
+
+function takeDamage() {
+  gameState.player.setTint(0xb700e0);
+  setTimeout(() => {
+    gameState.player.clearTint();
+  }, 200);
+  gameState.health -= 20;
+  console.log(gameState.health);
+  setHealth(gameState.health);
+}
+
+function setHealth(value) {
+  const width = 130;
+  let fillPercent = (Phaser.Math.Clamp(value, 0, 100) / 100) * width;
+  gameState.graphics.fillStyle(0x808080);
+  gameState.graphics.fillRoundedRect(20, 60, width, 10, 3);
+  gameState.graphics.fillStyle(0x00ff00);
+  gameState.graphics.fillRoundedRect(20, 60, fillPercent, 10, 3);
 }
